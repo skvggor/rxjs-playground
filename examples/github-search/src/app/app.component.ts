626b93692fb029460828b66240f4ce0264c728c7
Marcos Garcia de Lima <marckfree@gmail.com>
2018-07-30 18:28:39 -0300

    Add first example (WIP)

--- examples/github-search/src/app/app.component.ts
+++ examples/github-search/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { FormControl } from '@angular/forms'
+import { HttpClient } from '@angular/common/http'
+
+import { pipe, of, merge, from } from 'rxjs'
+import { map, filter, debounceTime, retry, switchMap, tap, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.styl']
 })
+
 export class AppComponent {
-  title = 'github-search';
+  title = 'Github Search'
+
+  timeout = 300
+  minChars = 3
+  isLoaded = false
+  maxAttempts = 3
+  
+  inputSearch = new FormControl('')
+
+  results = this.inputSearch.valueChanges
+    .pipe(
+      filter(value => value.length >= this.minChars),
+      debounceTime(this.timeout),
+      map(value => `https://api.github.com/search/repositories?q=${value}`),
+      retry(this.maxAttempts),
+      switchMap(url => this.http.get(url)),
+      map(json => json['items']),
+      tap(() => this.isLoaded = true)
+    )
+    .subscribe(items => this.results = items)
+
+  constructor(private http: HttpClient ) {}
 }
